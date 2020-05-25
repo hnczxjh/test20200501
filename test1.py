4d5fc7e4f82f5f44a97d2c8516292261ab1f858f
hnczxjh <57854748@qq.com>
2020-05-25 13:09:40 +0800

    permit2

--- test1.py
+++ test1.py
@@ -3,11 +3,10 @@ import pandas as pd
 import numpy as np
 
 data_file_name = os.path.join(os.path.expanduser('~'), 'PycharmProjects\\test2005\\Data\\Adult')
-adult = pd.read_csv(data_file_name, names=names = ["Age", "Work-Class", "fnlwgt",
-                                                   "Education", "Education-Num",
-                                                   "Marital-Status", "Occupation",
-                                                   "Relationship", "Race", "Sex",
-                                                   "Capital-gain", "Capital-loss",
-                                                   "Hours-per-week", "Native-Country",
-                                                   "Earnings-Raw"])
-
+adult = pd.read_csv(data_file_name, names=["Age", "Work-Class", "fnlwgt",
+                                           "Education", "Education-Num",
+                                           "Marital-Status", "Occupation",
+                                           "Relationship", "Race", "Sex",
+                                           "Capital-gain", "Capital-loss",
+                                           "Hours-per-week", "Native-Country",
+                                           "Earnings-Raw"])
